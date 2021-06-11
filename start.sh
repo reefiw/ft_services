262e78639c98744373ae0e6f5605dce3184416fb
Pinkman Lurlene <plurlene@pr-h2.kzn.21-school.ru>
2021-06-11 20:17:03 +0300

    wordpress ready

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ minikube delete
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 eval $(minikube docker-env)
-# docker pull metallb/speaker:v0.8.2
-# docker pull metallb/controller:v0.8.2
+docker pull metallb/speaker:v0.8.2
+docker pull metallb/controller:v0.8.2
 docker build -t nginx_image      ./nginx/
 docker build -t ftps_image       ./ftps/
 docker build -t mysql_image      ./mysql/
--- wordpress/srcs/wp-config-sample.php
+++ wordpress/srcs/wp-config-sample.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', 'phpmyadmin' );
 define( 'DB_HOST', 'mysql-svc' );
 
 /** Кодировка базы данных для создания таблиц. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** Схема сопоставления. Не меняйте, если не уверены. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_KEY',  'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_KEY',    'впишите сюда уникальную фразу' );
-define( 'NONCE_KEY',        'впишите сюда уникальную фразу' );
-define( 'AUTH_SALT',        'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_SALT', 'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_SALT',   'впишите сюда уникальную фразу' );
-define( 'NONCE_SALT',       'впишите сюда уникальную фразу' );
+define( 'AUTH_KEY',         '4_r+i~~2Z$7D&Bk]Yj-5K|5HI{WC4;sSDFS$;gh=FCK[0(`IUrD=3_I%5H={mWMT' );
+define( 'SECURE_AUTH_KEY',  '#B:`W6/=ZL%+@qc69$2B/E2F;|4Hx4[Yh3Rdg-EJ.MymJBmyJ3%2.EP--w=pjs*-' );
+define( 'LOGGED_IN_KEY',    'rUYceZu/L rkWpVpDuyV_CO:@<g7%*ja7l{ c81g@AYC` Pt9bC8@?L~v$T)/Q/)' );
+define( 'NONCE_KEY',        'PZ9<DPqO/i!pCSVW@?x,<%2/9|~2G;~$g=aR!33 A^f6O1sy`Ja*[?KL>(lJL3z9' );
+define( 'AUTH_SALT',        'e8>-y$X!9yZ^{[0K&j :!tNimZ65kc=r ;R6#Q#(trH FRk&{dnrZ@+IT[6sy2K$' );
+define( 'SECURE_AUTH_SALT', '}p^gME1xTv.Bqu)bX#9)D=R#1$~AUbu8El)Fa`B&M6I%;k&wXKeNM7 Um5zxkJM/' );
+define( 'LOGGED_IN_SALT',   'YH|.#@MBIrXp*B~p7Ku&Qu7GO9(nJTya&)uEEyO1<q:vaC{1XVYtOao5wUA_<>6J' );
+define( 'NONCE_SALT',       '-)%Tn81gutmVb;HLZJ(<.k).K 5=5$bTi5m/&->m~Rn.JO{w>mMMyQcXNDA@<9h2' );
 
 /**#@-*/
 
